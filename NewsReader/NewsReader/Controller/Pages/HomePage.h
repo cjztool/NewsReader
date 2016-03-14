408fbfad3370298b4ee52f80a5a9e906dd542b66
cjztool <cjztool@126.com>
2016-03-14 22:46:27 +0800

    增加基类

--- NewsReader/NewsReader/Controller/Pages/HomePage.h
+++ NewsReader/NewsReader/Controller/Pages/HomePage.h
@@ -6,9 +6,9 @@
 //  Copyright © 2016年 com.cjz. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "BasePage.h"
 
-@interface HomePage : UIViewController
+@interface HomePage : BasePage
 
 
 @end
